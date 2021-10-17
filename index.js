9479221997b1774d5b128c3f79b3eb231cf19081
Candice Smith MacKenzie <Mahony8@aol.com>
2021-10-17 01:02:07 -0400

    commit tree 62b3c5c42ac577cede593ef68adc08e868ed4b64

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function saturdayFun(activity='roller-skate'){
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+function mondayWork(toDoList='go to the office'){
+    return (`This Monday, I will ${toDoList}.`);
+}
+mondayWork();
+
+function wrapAdjective(special="*"){
+    let greet = "You are";
+      return function(greet2="a hard worker"){
+          return `${greet} ${special}${greet2}${special}!`;
+        }
+    };
+wrapAdjective("*");
+wrapAdjective("||")("a dedicated programmer");
+
 // Your code here
